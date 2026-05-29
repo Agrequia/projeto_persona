0d5212de08c5de4e4cfec751e0de195228e0f5ce
Agrequia <arthurrodrigueslima@gmail.com>
2026-05-28 22:38:31 -0300

    testes de como sera feito o sistema

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import random as rd
 
+def dv01(cpf):
+    soma = 0
+    for i in range(8):
+        soma = cpf[i] * i+1
+    print(soma)
+    return soma
+    
+
 cpf = [0,0,0,0,0,0,0,0,0,0,0]
 
-for i in cpf:
-    if i < 10:
-        cpf[i] = rd.randint(0, 9)
-        i = i + 1
+for i in range(8):
+    cpf[i] = rd.randint(0, 9)
 
 print(cpf)
+dv01(cpf)
